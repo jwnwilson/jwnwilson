d9c21f3cb6bcc36c0106c0251a9c2b43f36f24a2
Noel Wilson <jwnwilson@hotmail.co.uk>
2024-01-22 23:15:11 +0000

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import profile from './assets/profile.jpg'
 function App() {
 
   return (
-    <div className="flex h-screen">
-      <div className="m-auto border-2 border-slate-600 py-8 px-8 max-w-sm mx-auto bg-black rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-        <div className="block overflow-hidden mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0">
-          <img className="scale-125 block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={profile} alt="Noel's Face" />
+    <div className="flex h-screen bg-emerald-950">
+      <div className="relative m-auto border-2 border-black bg-zinc-900 py-8 px-8 max-w-sm mx-auto rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <div className="h-24">
+          <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={profile} alt="Noel's Face" />
         </div>
         <div className="text-center space-y-2 sm:text-left">
           <div className="space-y-0.5">
