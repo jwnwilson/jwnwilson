6c593c891ef39fd9759e02f0ee86a74558ec2a0f
Noel Wilson <jwnwilson@hotmail.co.uk>
2024-01-24 23:25:14 +0000

    Created simple cpu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css'
 import profile from './assets/profile.jpg'
 
-function App() {
-
+const Cpu = (props) => {
   return (
-    <div className="flex h-screen bg-emerald-950">
-      <div className="flex flex-col w-[250px] h-[250px] relative m-auto border-2 border-black bg-zinc-900 py-8 px-8 max-w-sm mx-auto rounded-xl shadow-lg space-y-2 items-center">
+    <div className="flex flex-col w-[250px] h-[250px] relative m-auto border-2 border-black bg-zinc-900 py-8 px-8 max-w-sm mx-auto rounded-xl shadow-lg space-y-2 items-center">
         <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "5px"}}></div>
         <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "20px", margin: 0}}></div>
         <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "35px", margin: 0}}></div>
@@ -38,6 +36,48 @@ function App() {
         <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "200px", margin: 0}}></div>
         <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "215px", margin: 0}}></div>
         <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "230px", margin: 0}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "0px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "15px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "30px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "45px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "60px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "75px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "90px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "105px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "120px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "135px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "150px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "165px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "180px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "195px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "210px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "225px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "0px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "15px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "30px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "45px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "60px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "75px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "90px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "105px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "120px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "135px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "150px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "165px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "180px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "195px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "210px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "225px"}}></div>
+        {props.children}
+      </div>
+  )
+}
+
+function App() {
+
+  return (
+    <div className="flex h-screen bg-emerald-950">
+      <Cpu>
         <div className="h-24 space-y-0.5">
           <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={profile} alt="Noel's Face" />
         </div>
@@ -51,7 +91,7 @@ function App() {
             </p>
           </div>
         </div>
-      </div>
+      </Cpu>
     </div>
   )
 }
