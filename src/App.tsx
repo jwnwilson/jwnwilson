4ea27afe823be0fd4269f8856f9d6359c2ab0402
Noel Wilson <jwnwilson@hotmail.co.uk>
2024-01-24 23:37:56 +0000

    looking at svgs for circuits

--- src/App.tsx
+++ src/App.tsx
@@ -4,72 +4,80 @@ import profile from './assets/profile.jpg'
 const Cpu = (props) => {
   return (
     <div className="flex flex-col w-[250px] h-[250px] relative m-auto border-2 border-black bg-zinc-900 py-8 px-8 max-w-sm mx-auto rounded-xl shadow-lg space-y-2 items-center">
-        <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "5px"}}></div>
-        <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "20px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "35px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "50px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "65px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "80px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "95px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "110px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "125px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "140px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "155px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "170px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "185px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "200px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "215px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -top-7 bg-zinc-400" style={{left: "230px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "5px"}}></div>
-        <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "20px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "35px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "50px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "65px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "80px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "95px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "110px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "125px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "140px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "155px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "170px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "185px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "200px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "215px", margin: 0}}></div>
-        <div className="absolute h-7 w-2 -bottom-7 bg-zinc-400" style={{left: "230px", margin: 0}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "0px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "15px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "30px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "45px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "60px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "75px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "90px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "105px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "120px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "135px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "150px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "165px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "180px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "195px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "210px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{left: "-28px", top: "225px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "0px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "15px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "30px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "45px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "60px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "75px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "90px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "105px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "120px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "135px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "150px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "165px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "180px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "195px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "210px"}}></div>
-        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{right: "-28px", top: "225px"}}></div>
+        <div className="absolute h-7 -top-7 bg-zinc-400" style={{width: "7px", left: "5px"}}></div>
+        <div className="absolute h-7 -top-7 bg-zinc-400" style={{width: "7px", left: "20px", margin: 0}}></div>
+        <div className="absolute h-7 -top-7 bg-zinc-400" style={{width: "7px", left: "35px", margin: 0}}></div>
+        <div className="absolute h-7 -top-7 bg-zinc-400" style={{width: "7px", left: "50px", margin: 0}}></div>
+        <div className="absolute h-7 -top-7 bg-zinc-400" style={{width: "7px", left: "65px", margin: 0}}></div>
+        <div className="absolute h-7 -top-7 bg-zinc-400" style={{width: "7px", left: "80px", margin: 0}}></div>
+        <div className="absolute h-7 -top-7 bg-zinc-400" style={{width: "7px", left: "95px", margin: 0}}></div>
+        <div className="absolute h-7 -top-7 bg-zinc-400" style={{width: "7px", left: "110px", margin: 0}}></div>
+        <div className="absolute h-7 -top-7 bg-zinc-400" style={{width: "7px", left: "125px", margin: 0}}></div>
+        <div className="absolute h-7 -top-7 bg-zinc-400" style={{width: "7px", left: "140px", margin: 0}}></div>
+        <div className="absolute h-7 -top-7 bg-zinc-400" style={{width: "7px", left: "155px", margin: 0}}></div>
+        <div className="absolute h-7 -top-7 bg-zinc-400" style={{width: "7px", left: "170px", margin: 0}}></div>
+        <div className="absolute h-7 -top-7 bg-zinc-400" style={{width: "7px", left: "185px", margin: 0}}></div>
+        <div className="absolute h-7 -top-7 bg-zinc-400" style={{width: "7px", left: "200px", margin: 0}}></div>
+        <div className="absolute h-7 -top-7 bg-zinc-400" style={{width: "7px", left: "215px", margin: 0}}></div>
+        <div className="absolute h-7 -top-7 bg-zinc-400" style={{width: "7px", left: "230px", margin: 0}}></div>
+        <div className="absolute h-7 -bottom-7 bg-zinc-400" style={{width: "7px", left: "5px"}}></div>
+        <div className="absolute h-7 -bottom-7 bg-zinc-400" style={{width: "7px", left: "20px", margin: 0}}></div>
+        <div className="absolute h-7 -bottom-7 bg-zinc-400" style={{width: "7px", left: "35px", margin: 0}}></div>
+        <div className="absolute h-7 -bottom-7 bg-zinc-400" style={{width: "7px", left: "50px", margin: 0}}></div>
+        <div className="absolute h-7 -bottom-7 bg-zinc-400" style={{width: "7px", left: "65px", margin: 0}}></div>
+        <div className="absolute h-7 -bottom-7 bg-zinc-400" style={{width: "7px", left: "80px", margin: 0}}></div>
+        <div className="absolute h-7 -bottom-7 bg-zinc-400" style={{width: "7px", left: "95px", margin: 0}}></div>
+        <div className="absolute h-7 -bottom-7 bg-zinc-400" style={{width: "7px", left: "110px", margin: 0}}></div>
+        <div className="absolute h-7 -bottom-7 bg-zinc-400" style={{width: "7px", left: "125px", margin: 0}}></div>
+        <div className="absolute h-7 -bottom-7 bg-zinc-400" style={{width: "7px", left: "140px", margin: 0}}></div>
+        <div className="absolute h-7 -bottom-7 bg-zinc-400" style={{width: "7px", left: "155px", margin: 0}}></div>
+        <div className="absolute h-7 -bottom-7 bg-zinc-400" style={{width: "7px", left: "170px", margin: 0}}></div>
+        <div className="absolute h-7 -bottom-7 bg-zinc-400" style={{width: "7px", left: "185px", margin: 0}}></div>
+        <div className="absolute h-7 -bottom-7 bg-zinc-400" style={{width: "7px", left: "200px", margin: 0}}></div>
+        <div className="absolute h-7 -bottom-7 bg-zinc-400" style={{width: "7px", left: "215px", margin: 0}}></div>
+        <div className="absolute h-7 -bottom-7 bg-zinc-400" style={{width: "7px", left: "230px", margin: 0}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", left: "-28px", top: "0px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", left: "-28px", top: "15px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", left: "-28px", top: "30px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", left: "-28px", top: "45px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", left: "-28px", top: "60px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", left: "-28px", top: "75px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", left: "-28px", top: "90px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", left: "-28px", top: "105px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", left: "-28px", top: "120px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", left: "-28px", top: "135px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", left: "-28px", top: "150px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", left: "-28px", top: "165px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", left: "-28px", top: "180px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", left: "-28px", top: "195px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", left: "-28px", top: "210px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", left: "-28px", top: "225px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", right: "-28px", top: "0px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", right: "-28px", top: "15px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", right: "-28px", top: "30px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", right: "-28px", top: "45px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", right: "-28px", top: "60px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", right: "-28px", top: "75px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", right: "-28px", top: "90px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", right: "-28px", top: "105px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", right: "-28px", top: "120px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", right: "-28px", top: "135px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", right: "-28px", top: "150px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", right: "-28px", top: "165px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", right: "-28px", top: "180px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", right: "-28px", top: "195px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", right: "-28px", top: "210px"}}></div>
+        <div className="absolute h-2 w-7 -top-1 bg-zinc-400" style={{height:"7px", right: "-28px", top: "225px"}}></div>
         {props.children}
       </div>
+  )
+}
+
+const Circuits = () =>{
+  return (
+    <svg>
+      <circle cx={50} cy={50} r={10} fill="red" />
+    </svg>
   )
 }
 
@@ -92,6 +100,7 @@ function App() {
           </div>
         </div>
       </Cpu>
+      {/* <Circuits></Circuits> */}
     </div>
   )
 }
