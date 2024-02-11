d12886cd39ac03daf752b9450afe14489c0b178f
Noel Wilson <jwnwilson@hotmail.co.uk>
2024-02-11 12:24:01 +0000

    Updating deps and tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 /** @type {import('tailwindcss').Config} */
-export default {
+export default withMT({
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        emerald: {
+          900: "#064e3b"
+        },
+        slate: {
+          500: "#64748b"
+        }
+      }
+    },
   },
   plugins: [],
-}
+})
 
